606781ef8d0038b98ae6b789758d32bf598d1cd9
Parvathy <parvathyvenu14@gmail.com>
2024-06-25 17:00:27 -0500

    Graded Assignment Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,15 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions=[
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3" ];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -26,25 +32,34 @@ candidateName = input.question("\n What is your Name?\n");
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-  candidateAnswer = input.question("\n Who was the first American woman in space? \n\t");
-
+  for(i=0; i < questions.length; i++){
+   
+    candidateAnswers.push (input.question(questions[i]));
+   
+       console.log(candidateAnswers);
+  }
+  
 }
 
-function gradeQuiz(candidateAnswers) {
-
+function gradeQuiz() {
+console.log(candidateAnswers);
+let correctAnswerCount = 0;
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (correctAnswer===candidateAnswer) {
-  console.log("Correct");
-}
-else {
-  console.log("Your answer is incorrect");
+for(let i=0; i < correctAnswers.length; i++){
+    if( correctAnswers[i] === candidateAnswers[i]){
+      console.log(`Question ${i+1}: Pass`);
+      correctAnswerCount ++;
+    }else {
+      console.log(`Question ${i+1}: Fail`);
 
+  }
 }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
-
+  
   return grade;
+
 }
 
 function runProgram() {
@@ -52,7 +67,7 @@ function runProgram() {
   // TODO 1.1c: Greet candidate using their name //
   console.log( `Hi ${candidateName}  Welcome to this quick questionnaire.\n`);
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  gradeQuiz();
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
